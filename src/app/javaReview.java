b55c33f7725b4dcb6b76f18c5bcb9678f05c2d63
John leo <ummercool559@gmail.com>
2026-09-04 14:28:36 +0800

    reviewed variable types

--- src/app/javaReview.java
+++ src/app/javaReview.java
@@ -2,6 +2,16 @@ package app;
 
 public class javaReview {
     public static void main(String[] args) {
-        System.out.println("Hi hello yeash");
+        String name = "John Leo Jariel";
+        int age = 22;
+        float weight = 70.9f;
+        char middleInitial = 'B';
+        boolean isPogi = true;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(middleInitial);
+        System.out.println(isPogi);
     }
 }
