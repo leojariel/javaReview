8ffa22275503fbe9b809fdf41ad5dfe20b2559d7
John leo <ummercool559@gmail.com>
2026-09-04 14:43:05 +0800

    learned typecasting

--- src/app/javaReview.java
+++ src/app/javaReview.java
@@ -2,16 +2,10 @@ package app;
 
 public class javaReview {
     public static void main(String[] args) {
-        String name = "John Leo Jariel";
-        int age = 22;
-        float weight = 70.9f;
-        char middleInitial = 'B';
-        boolean isPogi = true;
+        int maxScore = 500;
+        int userScore = 423;
 
-        System.out.println(name);
-        System.out.println(age);
-        System.out.println(weight);
-        System.out.println(middleInitial);
-        System.out.println(isPogi);
+        double percentage = (double) userScore / maxScore * 100.0d;
+        System.out.println(percentage);
     }
 }
