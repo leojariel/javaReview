94ff161d4abb4fd26f2fdd6e2a99a220f179ca43
John leo <ummercool559@gmail.com>
2026-09-04 15:15:29 +0800

    reviewer basic arithmetic

---
+++ src/app/Operators.java
@@ -0,0 +1,29 @@
+package app;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        // Basic Arithmetic
+
+        int num1 = 25;
+        int num2 = 15;
+
+        int sum = num1 + num2;
+        System.out.println("====== Addition ======");
+        System.out.println(num1 + " + " + num2 + " = " + sum);
+
+        int difference = num1 - num2;
+        System.out.println("====== Subtraction ======");
+        System.out.println(num1 + " - " + num2 + " = " + difference);
+
+        int product = num1 * num2;
+        System.out.println("====== Multiplication ======");
+        System.out.println(num1 + " * " + num2 + " = " + product);
+
+        int quotient = num1 * num2;
+        System.out.println("====== Division ======");
+        System.out.println(num1 + " / " + num2 + " = " + quotient);
+
+        //
+    }
+}
