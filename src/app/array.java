0e243339fc9ef14594df198a080a757bd196fb8d
John leo <ummercool559@gmail.com>
2026-09-04 17:40:23 +0800

    reviewed array

---
+++ src/app/array.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int[][] numbers = { {1, 2, 3}, {4, 5, 6} };
+
+        System.out.println(numbers[1][1]);
+    }
+}
